2fd8bb71fd14d98f7a91e683fa812145f4267383
Alden Sneath <alden@acsweb.com>
2023-05-11 06:18:14 +0200

    updated bananagram express

--- Bananagrams/BananaServer.js
+++ Bananagrams/BananaServer.js
@@ -3,25 +3,11 @@ const app = express();
 const port = 5000;
 
 const path = require('path');
-const fs = require('fs');
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/Bananagram.html'));
-});
-
-app.get('/Peel', function(req, res) {
-
-});
-
-app.get('/Dump', function(req, res) {
-    if (currentOffset + pageSize < wordlist.length) {
-        currentOffset += pageSize;
-    }
-    res.json(wordlist.slice(currentOffset, currentOffset + pageSize));
-});
-
-app.get('/Undo', function(req, res) {
-
 });
 
 let server = app.listen(port, function () {
